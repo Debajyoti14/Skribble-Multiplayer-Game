fee1b94a6c61575dc921c7ea4f684cf30dbc3dd2
Debajyoti <debajyotisaha14@gmail.com>
2022-06-05 22:00:21 +0530

    Backend setup Done

---
+++ lib/screens/paint_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class PaintScreen extends StatefulWidget {
+  const PaintScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PaintScreen> createState() => _PaintScreenState();
+}
+
+class _PaintScreenState extends State<PaintScreen> {
+  late IO.Socket _socket;
+
+  @override
+  void initState() {
+    super.initState();
+    connect();
+  }
+
+  //Socket io Connection
+  void connect() {
+    _socket = IO.io('http://192.168.0.106:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    _socket.connect();
+
+    //Listen to Socket
+    _socket.onConnect((data) => print(data));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("paint Screen"),
+    );
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var http = require('http');
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+const mongoose = require('mongoose');
+
+var io = require('socket.io')(server);
+
+//middleware
+app.use(express.json());
+
+// Connect to our Database
+const DB = `mongodb+srv://debajyoti14:7r4KqYSpwg4N0Tfx@cluster0.inq8lyq.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(DB).then(() => console.log('Connection Successful')).catch((e) => console.log(e))
+
+server.listen(port, "0.0.0.0", () => {
+    console.log(process.env.PASSWORD);
+
+    console.log('Server started & running on port' + port);
+})
