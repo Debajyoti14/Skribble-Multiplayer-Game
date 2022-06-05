315f85d504b1b3c1ed115574fae5a93810a2dbf3
Debajyoti <debajyotisaha14@gmail.com>
2022-06-05 21:55:39 +0530

    Backend setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skrible/screens/home_screen.dart';
+import 'package:skrible/screens/paint_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const PaintScreen(),
     );
   }
 }
